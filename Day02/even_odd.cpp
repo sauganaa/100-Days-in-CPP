44bc30f1e9ebca4337fea66ae7dbe95f45936560
sauganaa <spring@placeholder.com>
2026-07-23 18:54:21 -0700

    Day 2: Added even_odd.cpp

---
+++ Day02/even_odd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int num;
+    std::cout << "Enter an integer: ";
+    std::cin >> num;
+    if (num % 2 == 0)
+        std::cout << num << " is even." << std::endl;
+    else
+        std::cout << num << " is odd." << std::endl;
+    return 0;
+}
